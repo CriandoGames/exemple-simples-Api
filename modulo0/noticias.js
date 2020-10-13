b81dc26e27be98af19db16a5889bb6b9fca3ba73
Carlos Castro <carloscastrogames@gmail.com>
2020-10-13 14:58:37 -0300

    conceito velho de url

--- modulo0/noticias.js
+++ modulo0/noticias.js
@@ -1,10 +1,22 @@
 var http = require('http');
 
 
-var server = http.createServer(function(req,res){
+var server = http.createServer(function (req, res) {
     console.log("Criando uma api com NodeJS");
 
-    res.end("teste");    
+    var categoria = req.url;
+
+    if (categoria == '/tecnolgia') {
+        res.end("tecnolgia");
+    } else if (categoria == '/moda') {
+
+        res.end("moda");
+
+    } else {
+        res.end("pagina sem categoria");
+    }
+
+
 });
 
 server.listen(3000);
